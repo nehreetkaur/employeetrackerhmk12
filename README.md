045a2768ec7255d1ea7b3c396b3687b8e474c995
Nehreet Kaur <kaur.nehreet@gmail.com>
2021-01-11 15:21:22 -0600

    completed some functionality and README.md

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# employeetrackerhmk12
+# ABOUT
+
+In this application, managed the **DATA** of **EMPLOYEE** as 
+**EMPLOYEE TRACKER**. User is prompted with **EMPLOYEE** related 
+questions, and after answering the prompt questions certain required
+**TABLE** is generated . And also, **created** **DATABASE** and **TABLES**
+in **SQL WORKBENCH**.
+
+# TABLE OF CONTENTS
+
+[DETAILED-DESCRIPTION](#DETAILED-DESCRIPTION)
+
+[INSTALLATION](#INSTALLATION)
+
+[RESOURCES-REFERRED](#RESOURCES-REFERRED)
+
+[GIF-DEMO](#GIF-DEMO)
+
+[GIT-HUB-REPOSITORY](#GIT-HUB-REPOSITORY)
+
+
+# DETAILED-DESCRIPTION
+
+**USER** will run **node server.js** on terminal,
+will be presented with some choices  like **VIEWALLEMPLOYEE**,**ADDROLE** etc.
+After, selecting one of the choices, user will be presented with the **EMPLOYEE TABLE** as per user's selection. One has to install **npm i , sql ** to get all 
+the **DEPENDENCIES**. ALSO , need to create **EMPLOYEE-DB** in **SQL WORKBENCH**
+HAVE to create employee,role,department tables in the **EMPLOYEE-DB**.
+**CONNECTION** to the **DATABASE** in sql workbench is very important in **CODE** to move further in the working of this application. **WRITING QUERIES** to get recquired data from **EMPLOYEE-DB** in **CODE**.
+
+# INSTALLATION
+
+**npm i inquirer, sql**
+
+**sql workbench**
+
+**Vs Code**
+
+
+# **RESOURCES-REFERRED**
+
+[SQL](https://sqlbolt.com/lesson/introduction)
+
+[INQUIRER](https://www.npmjs.com/package/inquirer)
+
+[STACK-OVERFLOW](https://stackoverflow.com/questions/5706437/whats-the-difference-between-inner-join-left-join-right-join-and-full-join/6188334)
+
+
+# **VIDEO-DEMO**
+[VIDEO-DEMO](https://drive.google.com/file/d/1VpRzkYZA1K5J7FgAv-FjrdB7UWkij6Hz/view)
+
+
+# **GIT-HUB-REPOSITORY**
+
+
+[GIT_HUB-REPOSITORY](https://github.com/nehreetkaur/employeetrackerhmk12)
+
+
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function startapp() {
       "addDepartment",
       "addEmployee",
       "addRole",
-      "updateEmployeeByRole",
+      "updateEmployee",
       "deleteEmployee",
       "exit"
 
@@ -76,7 +76,9 @@ function startapp() {
 
       } else if (answer.options == "addRole") {
         addRole();
-      } else if (answer.options == "updateEmployeeByRole") {
+      } else if (answer.options == "addEmployee") {
+        addEmployee();
+      } else if (answer.options == "updateEmployee") {
         updateEmployee();
       } else if (answer.options == "deleteEmployee") {
         deleteEmployee();
@@ -111,8 +113,9 @@ const ViewAllDepartments = () => {
   connection.query("Select * FROM department", (err, res) => {
     if (err) throw err;
     console.table(res);
+    startapp();
   })
-  startapp();
+  //startapp();
 };
 
 
@@ -201,8 +204,9 @@ const ViewAllRoles = () => {
   connection.query("SELECT * FROM role", (err, res) => {
     if (err) throw err;
     console.table(res);
+    startapp();
   })
-  startapp();
+  //startapp();
 };
 
 
@@ -216,45 +220,7 @@ function updateEmployee() {
   let roleResults = [], employeeResults = []; // Initialize as empty arrays for now.
 
 
-  /* Nehreet, this is what I was saying in class. You can hardcode the results that come from MySQL server. But later we will need
-     to do 2 things. Query and get the results of all the 1)ROLES and 2)EMPLOYEES.
-     
-     Why do we need to do this?
-
-     Well its because we need:
-     1) The user to select the desired employee. Hence this is why we query all employees and present them the list of employees to choose
-    
-     2) The user to select new role for desired employee. We need to prompt the user with this as well.
-        Note: The new role is what we will use to update employee.
-  */
-
-  // Hard Coded Results from MySQL Server
-  // roleResults = [{
-  //     role_id: 1,
-  //     title: "TEACHING-ASSISTANT"
-  //   },
-  //   {
-  //     role_id: 2,
-  //     title: "TINSTRUCTOR"
-  //   }
-  // ];
-
-  // employeeResults = [
-  //   {
-  //     employee_id: 1,
-  //     first_name: "TONY",
-  //     last_name: "STARK",
-  //     role_id: 2
-  //   },
-  //   {
-  //     employee_id: 2,
-  //     first_name: "JOHN",
-  //     last_name: "DOE",
-  //     role_id: 1
-  //   }
-  // ];
-
-  // The following hardcoded code above is now replaced with the ROLES and EMPLOYEE queries ^^^^.
+ 
 
   // Query Employees
   connection.query(`SELECT * FROM employee`, function (err, res) {
@@ -282,39 +248,12 @@ function updateEmployee() {
         }
       ]).then(function (answer) {
 
-        /* 
-          At this point user has told us 2 things:
-
-          1) Desired employee
-          2) New role of employee
-
-          There is a slight problem we need to update the employee table, but that table requires role_id and employee_id.
-
-          So we need to write some logic to retrieve the IDs.
-
-          In the prompt above we only displayed role name and employee name. We did this so the user doesn't have to see or worry about IDs. Its more human readable to show them the role name and employee name instead.
-
-          The following logic gets employee_id and role_id:
-          */
+        
         let { employee_id } = employeeResults.find(employee => `${employee.first_name} ${employee.last_name}` === answer.employee_full_name);
 
         let { role_id } = roleResults.find(role => role.title === answer.role_title);
 
-        // ^^^ What is {employee_id} and {role_id}?
-        // Its called destructuring object. We can do this to simplify the code by only getting the 1 property we need from object.
-        // More details: 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring
-
-
-        // At this point we have the role_id and employee_id to filter on employee table. Now wee can update.
-
-        /*
-          Recall in class Nehreet you and I wrote this query in Workbench. We can now use the exact same query in our code.
-
-          UPDATE employee
-          SET role_id = 3
-          WHERE employee_id=1;
-        */
+        
         connection.query(`UPDATE employee SET role_id = ${role_id} WHERE employee_id=${employee_id}`, function (err, res) {
           if (err) throw err;
           console.log("Update Successful\n\n\n");
@@ -327,30 +266,21 @@ function updateEmployee() {
 
 }
 
+// only to view employess for delete function
+function viewEmp(){
+  console.log("hello");
+  connection.query(`SELECT * FROM employee 
+                    INNER JOIN role ON role.role_id=employee.role_id
+                    INNER JOIN department ON department.department_id=role.department_id`, (err, res) => {
 
-
-// delete a department
-function deleteEmployee() {
-  // console.log(" i am in delete Employee");
-  ViewAllEmployees();
-  connection.query("SELECT * FROM employee", function (err, res) {
     if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      deleteArray.push(res[i].employee_id);
-    }
-    inquirer.prompt({
-      name: "deleteEmployee",
-      type: "input",
-      message: "Enter the id of the employee you want to delete?",
-    }).then(function (answer) {
-      connection.query(`DELETE FROM employee WHERE employee_id="${answer.deleteEmployee}"`, function (err, res) {
-        if (err) throw err;
-        console.log("\n");
-        ViewAllEmployees();
-      });
-    })
-  })
+    console.log("\n");
+    console.table(res);
+
+})
 }
+
+
 
 
 
@@ -381,7 +311,7 @@ function addRole() {
 
       },
       {
-        name: "department_id",
+        name: "department_name",
         type: "list",
         message: "What is the departmentname for this role",
         choices: optionsArray
@@ -389,17 +319,105 @@ function addRole() {
       },
 
     ]).then(function (answer) {
-      for (i = 0; i < optionsArray.length; i++) {
-        if (optionsArray[i].name === answer.department_id) {
-          department_id = optionsArray[i].id;                              // after running inquirer on last option throwing me an error.
+      let department_id;
+
+      // res contains the deparment rows. We have to search for the row that has the department name that the user selected.
+      // Once we find that department name, then we use its department_id
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].name === answer.department_name) {
+          department_id = res[i].department_id;  
+          break;
         }
       }
-      connection.query(`INSERT INTO role (title ,salary , department_id) VALUES ('${answer.title}' , ${answer.salary}, ${department_id})`, (err, res) => {
-        if (err) throw err;
-        console.log("One role added" + res);
-        ViewAllRoles();
 
-      });
+      if (department_id) {
+        connection.query(`INSERT INTO role (title ,salary , department_id) VALUES ('${answer.title}' , ${answer.salary}, ${department_id})`, (err, res) => {
+          if (err) throw err;
+          console.log("One role added" + res);
+          ViewAllRoles();
+
+        });
+      }
     });
   })
+}
+
+
+function addEmployee() {
+
+
+  let optionsArray = [];
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      optionsArray.push(res[i].title);
+    }
+
+    inquirer.prompt([
+      {
+        name: "f_name",
+        type: "input",
+        message: "What is employee first_name",
+
+      },
+      {
+        name: "l_name",
+        type: "input",
+        message: "What is employee last_name",
+
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is the title for this role",
+        choices: optionsArray
+
+      },
+
+    ]).then(function (answer) {
+      let role_id;
+
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].title === answer.role) {
+          role_id = res[i].role_id;  
+          break;
+        }
+      }
+
+      if (role_id) {
+        connection.query(`INSERT INTO employee (first_name ,last_name , role_id) VALUES ('${answer.f_name}' , '${answer.l_name}', ${role_id})`, (err, res) => {
+          if (err) throw err;
+          console.log("One employee added" + res);
+          ViewAllEmployees();
+
+        });
+      }
+    });
+  })
+}
+
+
+
+// delete a department
+function deleteEmployee() {
+  console.log(" i am in delete Employee");
+ viewEmp();
+ connection.query("SELECT * FROM employee", function (err, res) {
+   if (err) throw err;
+   for (var i = 0; i < res.length; i++) {
+     deleteArray.push(res[i].employee_id);
+   }
+   inquirer.prompt({
+     
+     name: "deleteEmployee",
+     type: "input",
+     message: "Enter the id of the employee you want to delete?",
+   }).then(function (answer) {
+     connection.query(`DELETE FROM employee WHERE employee_id="${answer.deleteEmployee}"`, function (err, res) {
+       if (err) throw err;
+       console.log("\n");
+       ViewAllEmployees();
+     });
+   })
+ })
 }
